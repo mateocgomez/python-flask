ec707de514ddc955ec7cc34e6b6089f66dbb53cb
Herald Olivares <heraldmatias.oz@gmail.com>
2021-09-11 23:38:36 -0500

    Deployment ajustado para internacionalizacion

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/tutorial-canciones'));
 
+app.get('/en/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/en/index.html'));
+});
+
+app.get('/es/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/es/index.html'));
+});
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/es/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
